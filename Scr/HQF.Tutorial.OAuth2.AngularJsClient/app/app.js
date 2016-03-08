7cabecfd48c4dc115ed477ae948e9c7cde002dac
huoxudong125 <423huo@163.com>
2016-03-08 17:55:32 +0800

    Add AngularJs To Test OAuth2.0 Server and Resource Server.

---
+++ Scr/HQF.Tutorial.OAuth2.AngularJsClient/app/app.js
@@ -0,0 +1,69 @@
+﻿
+var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/app/views/orders.html"
+    });
+
+    $routeProvider.when("/refresh", {
+        controller: "refreshController",
+        templateUrl: "/app/views/refresh.html"
+    });
+
+    $routeProvider.when("/tokens", {
+        controller: "tokensManagerController",
+        templateUrl: "/app/views/tokens.html"
+    });
+
+    $routeProvider.when("/associate", {
+        controller: "associateController",
+        templateUrl: "/app/views/associate.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+
+});
+
+var authServiceBase = 'http://localhost:45834/';//Authorization Server Base URL
+//var serviceBase = 'http://ngauthenticationapi.azurewebsites.net/';
+
+var resourceServiceBase = 'http://localhost:46148/';//Resource Server Base URL
+
+
+app.constant('ngAuthSettings', {
+    apiServiceBaseUri: authServiceBase,
+    clientId: 'ngAuthApp'
+});
+
+app.constant('ngResourceSettings', {
+    apiServiceBaseUri: resourceServiceBase,
+    clientId: 'ngAuthApp'
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
+
+
